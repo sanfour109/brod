56367f793eb2aaf3ed6133a6b8adbf3f8fce2e09
sanfour109 <57115663+sanfour109@users.noreply.github.com>
2019-10-28 22:33:32 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,36 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+const prefix = "w!";
+var usage = `\`${prefix}bc <content>\``
+
+client.on('message' , message => {
+      if(message.author.bot) return;
+     
+      if(message.content.startsWith(prefix + "bcrole")) {
+        if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+        let args = message.content.split(" ").slice(2);
+     var codes = args.join(' ')
+       
+        if(!codes) {
+          message.channel.send("قم بكتابة الرسالة | `$rolebc role message`")
+            return;
+        }
+     
+     
+              var role = message.mentions.roles.first();
+                if(!role) {
+                  message.reply("لا توجد رتبة بهذا الاسم")
+                    return;
+                }
+            message.guild.members.filter(m => m.roles.get(role.id)).forEach(n => {
+              n.send(`${codes}`)
+            })
+            message.channel.send(`لقد تم ارسال هذه الرسالة الى ${message.guild.members.filter(m => m.roles.get(role.id)).size} عضو`)
+        }
+    });
+    
+    
+    
+    
+    client.login("")
